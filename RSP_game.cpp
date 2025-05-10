e3595289d51d9e627aba5250849a25c2ec0f2077
Min-asdf <sminjea12@gmail.com>
2025-05-10 10:10:15 +0900

    RSP_game.cpp

--- RSP_game.cpp
+++ RSP_game.cpp
@@ -1,32 +1,50 @@
+//헤더
 #include <iostream>
+//랜덤함수 쓰기 위한 헤더
 #include <ctime>
 
-char getUserChoice();
+//함수 선언
+//player RSP 선택 함수
+char getPlayerChoice();
+//computer RSP 선택 함수
 char getComputerChoice();
+//player, computer RSP 선택 보여주는 함수
 void showChoice(char choice);
+//승자 결정 함수
 void chooseWinner(char player, char computer);
 
+//메인 함수
 int main() {
+	//변수 선언
 	char player;
 	char computer;
 
-	player = getUserChoice();
+	std::cout << "Rock-Scissors-Paper Game!\n\n";
+	
+	//playerChoice 입력
+	player = getPlayerChoice();
+	//입력받은 playerChoice 보여줌
 	std::cout << "You choice: ";
 	showChoice(player);
 
+	//computerChoice 입력
 	computer = getComputerChoice();
 	std::cout << "Computer choice: ";
+	//입력받은 computerChoice 보여줌
 	showChoice(computer);
 
+	//승자 결정 함수
 	chooseWinner(player, computer);
 
 	return 0;
 }
 
-char getUserChoice() {
+//player RSP 선택 함수
+char getPlayerChoice() {
+	//변수 선언
 	char player;
-	std::cout << "Rock-Scissors-Paper Game!\n";
 
+	//player가 r, s, p 중 1개 입력할 때까지 반복
 	do {
 	std::cout << "Choose one of the following\n";
 	std::cout << "********************\n";
@@ -36,12 +54,17 @@ char getUserChoice() {
 	std::cin >> player;
 	}while(player != 'r' && player != 'p' && player != 's');
 
+	//입력된 RSP return
 	return player;
 }
+
+//computer RSP 선택 함수
 char getComputerChoice() {
+	//랜덤 함수
 	srand(time(0));
 	int num = rand() % 3 + 1;
 
+	//computer RSP return
 	switch (num) {
 	case 1: return 'r';
 	case 2: return 's';
@@ -50,7 +73,10 @@ char getComputerChoice() {
 
 	return 0;
 }
+
+//player, computer RSP 선택 보여주는 함수
 void showChoice(char choice) {
+	//player, computer 입력된 r, s, p 를 Rock, Scissors, Paper 로 바꿔서 보여줌
 	switch (choice) {
 	case 'r': std::cout << "Rock\n";
 		break;
@@ -60,8 +86,12 @@ void showChoice(char choice) {
 		break;
 	}
 }
+
+//승자 결정 함수(player, computer RSP 입력 받음)
 void chooseWinner(char player, char computer) {
+	//player RSP와 computer RSP를 받아와서 무승부, 패배, 승리 결정
 	switch (player) {
+		//player가 Rock을 선택했을 때
 		case 'r': if (computer == 'r') {
 			std::cout << "It's a tie!\n";
 		}
@@ -72,17 +102,19 @@ void chooseWinner(char player, char computer) {
 			std::cout << "You win!\n";
 		}
 				break;
-	case 's': if (computer == 'r') {
+		//player가 Scissors를 선택했을 때
+		case 's': if (computer == 'r') {
 			std::cout << "You lose!\n";
 		}
 				else if (computer == 'p') {
-			std::cout << "It's a tie!\n";
-		}
-				else {
 			std::cout << "You win!\n";
 		}
+				else {
+			std::cout << "It's a tie!\n";
+		}
 				break;
-	case 'p': if (computer == 'r') {
+		//player가 Paper을 선택했을 때
+		case 'p': if (computer == 'r') {
 			std::cout << "You win!\n";
 		}
 				else if (computer == 'p') {
